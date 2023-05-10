d73c9ed53c9e433e788bfc516e080e06b3f507df
Jonathan Lopez <jlopez0313@hotmail.com>
2023-05-09 20:35:58 -0500

    vercel

--- Server/server.js
+++ Server/server.js
@@ -24,7 +24,7 @@ class Server {
         this.paths = {
             auth: {
                 path: '/api/auth',
-                route: '../routes/auth.js',
+                route: './routes/auth',
             },
             especialidades: {
                 path: '/api/especialidades',
