ef55eec1aaed2a4646363fe9ab07c7673900652d
Jonathan Lopez <jlopez0313@hotmail.com>
2024-02-01 18:28:15 -0500

    testing

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const { dbConnection } = require('../database/config');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const { create, read } = require('../controllers/chat');
 
 class Server {
