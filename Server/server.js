c8d56073eadd29349529d7c0d0585c9fb0df5934
Jonathan Lopez <jlopez0313@hotmail.com>
2024-02-14 23:00:27 -0500

    formidable v2

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const { dbConnection } = require('../database/config');
 const cors = require('cors');
-const formidableMiddleware = require('express-formidable');
+const formidableMiddleware = require('express-formidable-v2');
 // const bodyParser = require('body-parser');
 const { create, read } = require('../controllers/chat');
 
